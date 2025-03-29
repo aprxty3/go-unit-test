c1078d85ace3cb2e0327d19f48194e7d9479eebc
aji prasetyo <ajiprxty3@gmail.com>
2025-03-29 23:11:16 +0700

    update

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,25 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
-	result := HelloWorld("Aji")
-	if result != "HelloAji" {
-		panic("Result is not Hello Aji")
+	result := HelloWorld("Enr")
+	if result != "Hello Enr" {
+		t.Error("Result must be Hello Enr")
 	}
 
+	fmt.Println("TestHelloWorld Done")
+
+}
+
+func TestHelloWorldAji(t *testing.T) {
+	result := HelloWorld("Aji")
+	if result != "Hello Aji" {
+		t.Fatal("Result must be Hello Aji")
+	}
+
+	fmt.Println("TestHelloWorldAji Done")
 }
